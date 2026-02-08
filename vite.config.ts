881ce051a94aee3183a43486a8c0bba76f455c3e
Kgaugelo Tladi <milkeyroise@gmail.com>
2026-02-08 09:44:14 +0200

    done

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -5,3 +6,19 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
+=======
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+})
+>>>>>>> Stashed changes
